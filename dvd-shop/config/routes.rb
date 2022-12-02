c41ddbead7d2cbaa1a6ebe57db0af96e77eee7fb
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-12-02 04:49:33 +0300

    updating our routes.rb file to setup just one route our frontend needs

--- dvd-shop/config/routes.rb
+++ dvd-shop/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :movies
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+ #updating our routes.rb file to set up just one route our frontend needs 
+ resources :movies, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
